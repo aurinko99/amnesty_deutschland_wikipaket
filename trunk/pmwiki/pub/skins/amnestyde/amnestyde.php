8c3773e89211d96998b3325619f473b541a2e9df
hombresiniestro <hombresiniestro@c1a68a6d-f329-4da9-aea3-385faf2fdb0f>
2009-07-06 19:31:44 +0000

    Versionsnummer 1.04
    git-svn-id: https://amnestyde-wiki.svn.sourceforge.net/svnroot/amnestyde-wiki@31 c1a68a6d-f329-4da9-aea3-385faf2fdb0f

--- trunk/pmwiki/pub/skins/amnestyde/amnestyde.php
+++ trunk/pmwiki/pub/skins/amnestyde/amnestyde.php
@@ -1,7 +1,7 @@
 <?php if (!defined('PmWiki')) exit();
 /*
  * PmWiki amnestyde skin
- * Version 1.0.3  (05.03.2009)
+ * Version 1.0.4  (06.07.2009)
  * @requires PmWiki 2.2
  *
  * Copyright (c) 2008 Amnesty International, Deutsche Sektion
@@ -10,7 +10,7 @@ global $FmtPV, $SkinName, $SkinVersionDate, $SkinVersionNum, $SkinVersion, $Skin
        $SkinSourceURL, $RecipeInfo;
 $SkinName = 'amnestyde';
 $SkinRecipeName = "AmnestyDe";
-$RecipeInfo['amnestyde']['Version'] = '2009-03-05 (1.03)';
+$RecipeInfo['amnestyde']['Version'] = '2009-07-06 (1.04)';
 
 ## Add a custom page storage location
 global $PageStorePath, $WikiLibDirs;
