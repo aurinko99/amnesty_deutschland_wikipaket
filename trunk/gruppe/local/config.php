d60a117c21cf1d827eb06674cabd920978b9cf95
hombresiniestro <hombresiniestro@c1a68a6d-f329-4da9-aea3-385faf2fdb0f>
2009-07-17 10:46:34 +0000

    local/pub-dir entfernt
    git-svn-id: https://amnestyde-wiki.svn.sourceforge.net/svnroot/amnestyde-wiki@50 c1a68a6d-f329-4da9-aea3-385faf2fdb0f

--- trunk/gruppe/local/config.php
+++ trunk/gruppe/local/config.php
@@ -2,7 +2,6 @@
 ## Use "Clean URLs".
 $EnablePathInfo = 1;
 $ScriptUrl = "http://www.amnesty-orscholz.de";
-$PubDirUrl = 'http://www.amnesty-orscholz.de/pub';
 
 $DefaultPasswords['admin'][] = crypt('mate-club');
 $DefaultPasswords['edit'] = crypt('club-mate');
